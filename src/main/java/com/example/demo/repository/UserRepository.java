ecbb1e4237c2c22dbdabe58466a80da985599e09
entykey <nguyentuan4026@gmail.com>
2025-11-07 10:53:59 +0700

    fix xung đột method giữa interface UserRepository và JpaRepository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,6 +9,5 @@ public interface UserRepository {
     List<User> findAll();
     Optional<User> findById(Long id);
     User save(User user);
-    void delete(Long id);
     void deleteById(Long id);
 }
